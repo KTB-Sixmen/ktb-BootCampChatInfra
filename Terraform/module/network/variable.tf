a715d883095d95a72f91c8932a930983dfa97574
aida <ghk01214@naver.com>
2024-12-09 18:33:22 +0900

    terraform infra 세팅 중

---
+++ Terraform/module/network/variable.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidrs" {
+  description = "CIDR blocks for the subnets"
+  type        = list(string)
+}
+
+variable "subnet_names" {
+  description = "Names for the subnets"
+  type        = list(string)
+}
+
+variable "any_ip" {
+  description = "IP address for anywhere"
+  type        = string
+}
